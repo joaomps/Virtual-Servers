0dd10d4512874968aea584d49992f17bd0098fcd
joaomps <joaomps@outlook.pt>
2015-12-18 19:03:21 +0000

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -7,20 +7,7 @@
 sudo apt-get purge apache2* bind9* samba* -y
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install nano xorg lxde-core firefox tightvncserver gedit -y
+sudo apt-get install nano xorg lxde-core firefox tightvncserver -y
 sudo apt-get update -y
-clear
-echo ""
-echo "Enter a password for VNC"
-tightvncserver && tightvncserver -kill :80
-sed -i 'lxterminal &' ~/.vnc/xstartup
-sed -i '/usr/bin/lxsession -s LXDE &' ~/.vnc/xstartup
 wget https://raw.githubusercontent.com/joaomps/Virtual-Servers/master/script.sh -o /root/Desktop
-vncserver :80 -geometry 1024x768 -depth 16 -pixelformat rgb565
-clear
-echo ""
-echo "Connect to the server at  $IP:80 or $IP:80"
-echo ""
-echo "if asked click use default config"
-echo ""
-fi
+
