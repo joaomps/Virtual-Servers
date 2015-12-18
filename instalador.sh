299bd2ec7849da06ac77666aa9fd183698897bc5
joaomps <joaomps@outlook.pt>
2015-12-18 09:43:02 +0000

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 #########################################################################################
 # Created by DjRaPmAn                                                                   #
-#                                                                                       #
-# This Script will download and install needed packages for a working desktop           #
-# environment Due to bugs in centos 6 XFCE doesn't work correctly over VNC, so          #
-# the script will jest install the "Desktop" group package.                             #
-#                                                                                       #
-#                                                                                       #
-# Also since this is a server I recomend securing it even do it is only used for bot's  #
-# it still is a server. Here a good read up / tutorial on how to do so                  #
-# http://wiki.centos.org/HowTos/OS_Protection                                           #
-#                                                                                       #
-# You may edit or modify or redistribute this script.                                   #
-# Version 2.01                                                                          #
-# 10 November, 2012                                                                     #
+# Works  on Ubuntu 14.04 x64                                                            #
 #########################################################################################
 
 sudo apt-get purge apache2* bind9* samba* -y
