cf96523d7338cdaa30010a3650d2cfba62dbdd93
joaomps <joaomps@outlook.pt>
2015-12-29 14:43:01 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,6 +1,6 @@
 while true
 do
-  firefox http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60
+  firefox http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60 &
   sleep 3600
   pkill -f -9 firefox
 done
