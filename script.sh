73de44f4281b844b7b35c154a004c124c3cda868
joaomps <joaomps@outlook.pt>
2016-01-02 19:06:59 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-while true; do
-  chromium "http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60"  --disable-session-crashed-bubble --no-sandbox --user-data-dir=/home/chrome/ &
-  chromium_pid=$!
+while true
+do
+  chromium http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60 &
   sleep 3600
-  kill $chromium_pid
+  pkill -f -9 chromium
 done
