fa3eea79b66b1b21701937e6a87250f604b7256f
joaomps <joaomps@outlook.pt>
2016-01-02 16:14:52 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 while true; do
-  DISPLAY=:1 chromium "http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60"  --disable-session-crashed-bubble --no-sandbox --user-data-dir=/home/chrome/ &
+  chromium "http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60"  --disable-session-crashed-bubble --no-sandbox --user-data-dir=/home/chrome/ &
   chromium_pid=$!
   sleep 3600
   kill $chromium_pid
