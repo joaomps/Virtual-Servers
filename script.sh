636674679e69ac58f566f3486475e0a0caeec731
joaomps <joaomps@outlook.pt>
2016-01-02 15:55:55 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,6 @@
-#!/bin/bash
-# http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60 #
-while true
-do
-for ((z=1;z<=60;z++))
-do
-if pgrep "firefox" > /dev/null
-then
-echo "firefox is running"
-else
-echo "restarting firefox"
-firefox
-fi
-sleep 60
-done
-pkill firefox
+while true; do
+  DISPLAY=:1 chromium "http://22hits.com/exchange.php?key=c78e7d9e6a6fe1ed068f2362b908ba2ad07e05984d8c00ad8c6514b4354e5e60"  --disable-session-crashed-bubble --no-sandbox --user-data-dir=/home/chrome/ &
+  chromium_pid=$!
+  sleep 3600
+  kill $chromium_pid
 done
